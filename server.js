8604b8e68d6515a87c3be708837b34b40dfd3cde
manzoor <manzoor8132@gmail.com>
2026-02-25 13:45:45 +0500

    Fix: read PORT from env instead of hardcoded 4000

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
 console.log('AWS Config:', {
